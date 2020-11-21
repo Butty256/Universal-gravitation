862307006468ad1e7a7df36ceafdf3f68a47cb78
Butty256 <butty.main@gmail.com>
2020-11-21 09:08:46 +0900

    base ok

--- main.c
+++ main.c
@@ -15,10 +15,32 @@
 #define GC 6.674e-11
 
 /********************************
+	構造体
+********************************/
+
+struct materialStruct
+{
+	GLfloat ambient[4], diffuse[4], specular[4];
+	GLfloat shininess;
+};
+typedef struct materialStruct materialStruct;
+
+struct Object
+{
+	GLdouble m;
+	GLdouble p[3];
+	GLdouble v[3];
+	GLdouble r;
+	materialStruct c;
+};
+typedef struct Object Object;
+
+/********************************
 	関数のプロトタイプ宣言
 ********************************/
 
 void display(void);
+void materials(materialStruct m);
 void saveImage(void);
 static void timer(int dummy);
 void resize(int w, int h);
@@ -29,27 +51,15 @@ void mouse(int button, int state, int x, int y);
 void init(void);
 
 /********************************
-	構造体
-********************************/
-
-struct Object
-{
-	GLUquadric *o;
-	GLdouble m;
-	GLdouble p[3];
-	GLdouble v[3];
-	GLdouble r;
-	GLfloat c[3];
-};
-typedef struct Object Object;
-
-/********************************
 	グローバル変数
 ********************************/
 
 /* 物体 */
 Object g_obj[DATANUM];
 int g_onum;
+
+/* Quadric */
+GLUquadric *g_quad;
 
 /* 万有引力定数 */
 GLdouble g_gc = GC;
@@ -63,8 +73,8 @@ GLdouble g_ax = 0.0, g_ay = 0.0, g_az = 0.0;
 GLdouble g_ux = 0.0, g_uy = 0.0, g_uz = 1.0;
 
 /* 描画サイズ */
-GLdouble g_size = 700.0;
-GLdouble g_xscale = 0.1, g_yscale = 0.1, g_zscale = 0.1;
+GLdouble g_size = 10.0;
+GLdouble g_xscale = 1.0, g_yscale = 1.0, g_zscale = 1.0;
 
 /* マウスの角度 */
 int g_mx, g_my;
@@ -87,7 +97,6 @@ void display(void)
 		g_xscale * g_ax, g_yscale * g_ay, g_zscale * g_az,
 		g_ux, g_uy, g_uz
 	);
-	glScaled(g_xscale, g_yscale, g_zscale);
 
 	for (i = 0; i < g_onum; i++)
 	{
@@ -97,6 +106,15 @@ void display(void)
 	}
 
 	glutSwapBuffers();
+}
+
+/* マテリアル */
+void materials(materialStruct m)
+{
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m.ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, m.diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m.specular);
+	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, m.shininess);
 }
 
 /* 画像保存関数 */
@@ -148,9 +166,9 @@ static void timer(int dummy)
 
 	for (i = 0; i < g_onum; i++)
 	{
-		g_obj[i].p[0] += g_obj[i].v[0] * (g_dt / 1000);
-		g_obj[i].p[1] += g_obj[i].v[1] * (g_dt / 1000);
-		g_obj[i].p[2] += g_obj[i].v[2] * (g_dt / 1000);
+		g_obj[i].p[0] += g_obj[i].v[0] * 0.001;
+		g_obj[i].p[1] += g_obj[i].v[1] * 0.001;
+		g_obj[i].p[2] += g_obj[i].v[2] * 0.001;
 	}
 
 	for (i = 0; i < g_onum; i++)
@@ -162,18 +180,12 @@ static void timer(int dummy)
 			y = g_obj[j].p[1] - g_obj[i].p[1];
 			z = g_obj[j].p[2] - g_obj[i].p[2];
 			r = sqrt(x * x + y * y + z * z);
-			if (r >= g_obj[i].r + g_obj[j].r)
+			if (0.5 * r >= g_obj[i].r + g_obj[j].r)
 			{
-				g_obj[i].v[0] += g_gc * g_obj[j].m / (r * r) * (x / r) * (g_dt / 1000);
-				g_obj[i].v[1] += g_gc * g_obj[j].m / (r * r) * (y / r) * (g_dt / 1000);
-				g_obj[i].v[2] += g_gc * g_obj[j].m / (r * r) * (z / r) * (g_dt / 1000);
+				g_obj[i].v[0] += g_gc * g_obj[j].m / (r * r) * (x / r) * 0.001;
+				g_obj[i].v[1] += g_gc * g_obj[j].m / (r * r) * (y / r) * 0.001;
+				g_obj[i].v[2] += g_gc * g_obj[j].m / (r * r) * (z / r) * 0.001;
 			}
-			//else
-			//{
-			//	g_obj[i].v[0] *= -1;
-			//	g_obj[i].v[1] *= -1;
-			//	g_obj[i].v[2] *= -1;
-			//}
 		}
 	}
 
@@ -200,10 +212,8 @@ void keyin(unsigned char key, int x, int y)
 		case 'Y': g_yscale /= 1.2; glutPostRedisplay(); break;
 		case 'z': g_zscale *= 1.2; glutPostRedisplay(); break;
 		case 'Z': g_zscale /= 1.2; glutPostRedisplay(); break;
-		case 'g': g_gc *= 10; glutPostRedisplay(); break;
-		case 'G': g_gc /= 10; glutPostRedisplay(); break;
-		case 't': g_dt *= 1.2; glutPostRedisplay(); break;
-		case 'T': g_dt /= 1.2; glutPostRedisplay(); break;
+		case 't': g_dt /= 1.2; glutPostRedisplay(); break;
+		case 'T': g_dt *= 1.2; glutPostRedisplay(); break;
 		case 'i': g_dt *= -1; glutPostRedisplay(); break;
 		case 'u': g_size *= 1.05; resize(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)); glutPostRedisplay(); break;
 		case 'U': g_size /= 1.05; resize(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)); glutPostRedisplay(); break;
@@ -264,21 +274,24 @@ void mouse(int button, int state, int x, int y)
 void init(void)
 {
 	int i;
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glEnable(GL_DEPTH_TEST);
 
 	g_ex = sin(g_mt) * cos(g_mp);
 	g_ey = sin(g_mt) * sin(g_mp);
 	g_ez = cos(g_mt);
 
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	g_quad = gluNewQuadric();
+	gluQuadricDrawStyle(g_quad, GLU_FILL);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
 	for (i = 0; i < g_onum; i++)
 	{
-		g_obj[i].o = gluNewQuadric();
-		//gluQuadricDrawStyle(g_obj[i].o, GLU_LINE);
 		glNewList(i + 1, GL_COMPILE);
-		glColor3fv(g_obj[i].c);
 		glPushMatrix();
-		gluSphere(g_obj[i].o, g_obj[i].r, 16, 16);
+		materials(g_obj[i].c);
+		gluSphere(g_quad, g_obj[i].r, 16, 16);
 		glPopMatrix();
 		glEndList();
 	}
@@ -287,6 +300,7 @@ void init(void)
 /* メイン関数 */
 int main(int argc, char **argv)
 {
+	GLfloat r, g, b;
 	char buf[64];
 
 	fprintf(stdout, "m px py pz vx vy vz r R G B\nend command: 0\n");
@@ -303,15 +317,28 @@ int main(int argc, char **argv)
 		g_obj[g_onum].v[1] = atof(strtok(NULL, " \0"));
 		g_obj[g_onum].v[2] = atof(strtok(NULL, " \0"));
 		g_obj[g_onum].r = atof(strtok(NULL, " \0"));
-		g_obj[g_onum].c[0] = atof(strtok(NULL, " \0"));
-		g_obj[g_onum].c[1] = atof(strtok(NULL, " \0"));
-		g_obj[g_onum].c[2] = atof(strtok(NULL, " \0"));
+		r = atof(strtok(NULL, " \0"));
+		g = atof(strtok(NULL, " \0"));
+		b = atof(strtok(NULL, " \0"));
+		g_obj[g_onum].c.ambient[0] = 0.3 * r;
+		g_obj[g_onum].c.ambient[1] = 0.3 * g;
+		g_obj[g_onum].c.ambient[2] = 0.3 * b;
+		g_obj[g_onum].c.ambient[3] = 1;
+		g_obj[g_onum].c.diffuse[0] = 0.6 * r;
+		g_obj[g_onum].c.diffuse[1] = 0.6 * g;
+		g_obj[g_onum].c.diffuse[2] = 0.6 * b;
+		g_obj[g_onum].c.diffuse[3] = 1;
+		g_obj[g_onum].c.specular[0] = 0.2 * r + 0.6;
+		g_obj[g_onum].c.specular[1] = 0.2 * g + 0.6;
+		g_obj[g_onum].c.specular[2] = 0.2 * b + 0.6;
+		g_obj[g_onum].c.specular[3] = 1;
+		g_obj[g_onum].c.shininess = 32;
 	}
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowSize(700, 700);
-	glutCreateWindow(argv[1]);
+	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
 	glutReshapeFunc(resize);
 	glutTimerFunc(g_dt, timer, 0);
